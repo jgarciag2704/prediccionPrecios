1253d891cf716d39037619abdd92e8eeb377ca5b
jgarciag <jgarciag@jumapacelaya.com.mx>
2025-02-05 10:08:31 -0600

    estructuraInicial

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class historicoPrecios (models.Model):
+    Fecha=models.DateField()
+    Nombre=models.TextField()
+    Calidad=models.TextField()
+    Presentacion=models.TextField()
+    Origen=models.TextField()
+    mercadoDeAbastos=models.TextField()
+    precioMinimo=models.DecimalField(max_digits=7,  # 9999.99 (hasta 4 enteros y 2 decimales)
+        decimal_places=2,validators=[MinValueValidator(0)],  # Evita precios negativos
+    )
+    precioMaximo=models.DecimalField(max_digits=7,  
+        decimal_places=2,validators=[MinValueValidator(0)],  
+    )
+    preciopromedio=models.DecimalField(max_digits=7, 
+        decimal_places=2,validators=[MinValueValidator(0)],  
+    )
+    
+    def __str__(self):
+        return f"{self.verdura} - ${self.precio} ({self.fecha})"
--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import historicoPrecios
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
+
+def index(request):
+    title="Prediccion de verduras"
+    return render(request,"index.html",{
+        'title':title
+    })
+    
+def about(request):
+    username='Jose Manuel'
+    return render(request,"about.html",{
+        'username':username
+    })
